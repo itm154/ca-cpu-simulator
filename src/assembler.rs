c2b31a6826a8a91f405256a1c58139524362bc58
itm154 <ashrulfahmi@gmail.com>
2025-05-31 22:57:19 +0800

    It works lmao

--- src/assembler.rs
+++ src/assembler.rs
@@ -2,6 +2,24 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Read, Write};
 
+enum OpCode {
+    HALT = 0b0000,
+    LVAL = 0b0001,  // Load immediate value into register
+    LOAD = 0b0011,  // Load value from memory into register
+    STORE = 0b0100, // Store value of register to memory
+    ADD = 0b0101,
+    SUB = 0b0110,
+    JMP = 0b0111,
+    MOV = 0b1000, // Move register's value to another register
+}
+
+enum Register {
+    R0 = 0b0000,
+    R1 = 0b0001,
+    R2 = 0b0010,
+    R3 = 0b0011,
+}
+
 fn main() {
     let mut args = std::env::args();
     let filename = args.nth(1).unwrap();
@@ -24,8 +42,47 @@ fn main() {
                 let (instruction, _) = line.split_once(char::is_whitespace).unwrap();
 
                 match instruction {
-                    "HALT" => {}
-                    "LVAL" => {}
+                    "HALT" => {
+                        let binary_instruction = (OpCode::HALT as u16) << 12;
+
+                        output_file
+                            .write(binary_instruction.to_be_bytes().as_slice())
+                            .unwrap();
+                    }
+                    "LVAL" => {
+                        let (_, args) = line.split_once(char::is_whitespace).unwrap();
+                        let (arg1, arg2) = args.split_once(", ").unwrap();
+                        let mut binary_instructions = (OpCode::LVAL as u16) << 12;
+
+                        match arg1 {
+                            "R0" => {
+                                binary_instructions =
+                                    binary_instructions | (Register::R0 as u16) << 8;
+                            }
+                            "R1" => {
+                                binary_instructions =
+                                    binary_instructions | (Register::R1 as u16) << 8;
+                            }
+                            "R2" => {
+                                binary_instructions =
+                                    binary_instructions | (Register::R2 as u16) << 8;
+                            }
+                            "R3" => {
+                                binary_instructions =
+                                    binary_instructions | (Register::R3 as u16) << 8;
+                            }
+                            _ => {
+                                panic!("Unknown register {arg1}")
+                            }
+                        }
+
+                        binary_instructions =
+                            binary_instructions | arg2.to_string().parse::<u16>().unwrap();
+
+                        output_file
+                            .write(binary_instructions.to_be_bytes().as_slice())
+                            .unwrap();
+                    }
                     "LOAD" => {}
                     "STORE" => {}
                     "ADD" => {}
